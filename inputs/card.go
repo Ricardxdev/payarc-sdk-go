4ee559539a40cd8c7081bd696df43696eccfd348
Ricardo Martinez <ricardoxdev@gmail.com>
2025-02-26 00:40:14 -0400

    add missing json annotations to CreateTokenDTO and UpdateCardDTO

--- inputs/card.go
+++ inputs/card.go
@@ -8,29 +8,29 @@ type CreateCardDTO struct {
 }
 
 type CreateTokenDTO struct {
-	CardSource     string        `json:"card_source"`
-	CardNumber     string        `json:"card_number"`
-	ExpMonth       int           `json:"exp_month"`
-	ExpYear        int           `json:"exp_year"`
-	CVV            int           `json:"cvv"`
-	CardHolderName string        `json:"card_holder_name"`
-	AddressLine1   string        `json:"address_line1"`
-	AddressLine2   string        `json:"address_line2"`
-	City           string        `json:"city"`
-	State          string        `json:"state"`
-	ZIPCode        string        `json:"zip"`
-	Country        string        `json:"country"`
-	AuthorizeCard  extra.Boolean `json:"authorize_card"`
+	CardSource     string        `json:"card_source,omitempty"`
+	CardNumber     string        `json:"card_number,omitempty"`
+	ExpMonth       int           `json:"exp_month,omitempty"`
+	ExpYear        int           `json:"exp_year,omitempty"`
+	CVV            int           `json:"cvv,omitempty"`
+	CardHolderName string        `json:"card_holder_name,omitempty"`
+	AddressLine1   string        `json:"address_line1,omitempty"`
+	AddressLine2   string        `json:"address_line2,omitempty"`
+	City           string        `json:"city,omitempty"`
+	State          string        `json:"state,omitempty"`
+	ZIPCode        string        `json:"zip,omitempty"`
+	Country        string        `json:"country,omitempty"`
+	AuthorizeCard  extra.Boolean `json:"authorize_card,omitempty"`
 }
 
 type UpdateCardDTO struct {
-	ExpMonth       int    `json:"exp_month"`
-	ExpYear        int    `json:"exp_year"`
-	CardHolderName string `json:"name"`
-	AddressLine1   string `json:"address_line1"`
-	AddressLine2   string `json:"address_line2"`
-	City           string `json:"city"`
-	StateCode      string `json:"state_code"`
-	ZIPCode        int    `json:"zip"`
-	CountryCode    string `json:"country_code"`
+	ExpMonth       int    `json:"exp_month,omitempty"`
+	ExpYear        int    `json:"exp_year,omitempty"`
+	CardHolderName string `json:"name,omitempty"`
+	AddressLine1   string `json:"address_line1,omitempty"`
+	AddressLine2   string `json:"address_line2,omitempty"`
+	City           string `json:"city,omitempty"`
+	StateCode      string `json:"state_code,omitempty"`
+	ZIPCode        int    `json:"zip,omitempty"`
+	CountryCode    string `json:"country_code,omitempty"`
 }
